d07147b6b64f05d72e955237dbea6d87c9fa5c58
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2026-03-19 13:25:31 +0100

    last commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
+<!-- These are examples of badges you might want to add to your README:
+     please update the URLs accordingly
+
+[![Built Status](https://api.cirrus-ci.com/github/<USER>/uniovi-simur-wearablepermed-predictor.svg?branch=main)](https://cirrus-ci.com/github/<USER>/uniovi-simur-wearablepermed-predictor)
+[![ReadTheDocs](https://readthedocs.org/projects/uniovi-simur-wearablepermed-predictor/badge/?version=latest)](https://uniovi-simur-wearablepermed-predictor.readthedocs.io/en/stable/)
+[![Coveralls](https://img.shields.io/coveralls/github/<USER>/uniovi-simur-wearablepermed-predictor/main.svg)](https://coveralls.io/r/<USER>/uniovi-simur-wearablepermed-predictor)
+[![PyPI-Server](https://img.shields.io/pypi/v/uniovi-simur-wearablepermed-predictor.svg)](https://pypi.org/project/uniovi-simur-wearablepermed-predictor/)
+[![Conda-Forge](https://img.shields.io/conda/vn/conda-forge/uniovi-simur-wearablepermed-predictor.svg)](https://anaconda.org/conda-forge/uniovi-simur-wearablepermed-predictor)
+[![Monthly Downloads](https://pepy.tech/badge/uniovi-simur-wearablepermed-predictor/month)](https://pepy.tech/project/uniovi-simur-wearablepermed-predictor)
+[![Twitter](https://img.shields.io/twitter/url/http/shields.io.svg?style=social&label=Twitter)](https://twitter.com/uniovi-simur-wearablepermed-predictor)
+-->
+
+[![Project generated with PyScaffold](https://img.shields.io/badge/-PyScaffold-005CA0?logo=pyscaffold)](https://pyscaffold.org/)
+
 # uniovi-simur-wearablepermed-predictor
-Uniovi Simur WearablePerMed Predictor
+
+> Uniovi Simur WearablePerMed Predictor.
+
+A longer description of your project goes here...
+
+
+<!-- pyscaffold-notes -->
+
+## Note
+
+This project has been set up using PyScaffold 4.6. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ src/wearablepermed_predictor/main.py
@@ -0,0 +1,185 @@
+import os
+import sys
+import argparse
+import logging
+from pathlib import Path
+import joblib
+from pyaml_env import parse_config
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from wearablepermed_pipeline import pipeline
+
+__author__ = "Miguel Salinas Gancedo"
+__copyright__ = "Miguel Salinas Gancedo"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+def argument_spaces(arg):
+    return arg.split(',')
+
+def parse_args(args):   
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Predictor Job")
+    parser.add_argument(
+        "-resource-id",
+        "--resource-id",
+        dest="resource_id",
+        help="Resource Id"
+    )
+    parser.add_argument(
+        "-model-id",
+        "--model-id",
+        dest="model_id",
+        help="Model Id"
+    )
+    parser.add_argument(
+        "-resource-file",
+        "--resource-file",
+        dest="resource_file",
+        help="resource file [csv]"
+    )    
+    parser.add_argument(
+        "-user-email",
+        "--user-email",
+        dest="user_email",
+        help="User email"
+    )               
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )   
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    return parser.parse_args(args)
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+def predict_by_mock(config, resource_file):
+    # Create a list of mock Prediction objects
+    mock_prediction = {}
+
+    return mock_prediction
+
+def load_model(model_id, base_path):
+    # 1. Reconstruct the exact same path used in store()
+    file_path = os.path.join(base_path, 'models', model_id)
+    
+    # 2. Load the model from the disk
+    if os.path.exists(file_path):
+        model = joblib.load(file_path)
+
+        return model
+    else:
+        print("Error: Model file not found.")
+        return None
+
+def predict_by_resource(resource_file, model_id):    
+    predictions = []
+    #predictions = predict(prediction_file, resource_file)
+
+    return predictions
+
+def send_email(args, config, resource_path, receiver_email):
+    # SMTP server config
+    smtp_host = config.host
+    smtp_port = config.port
+    sender_email = config.sender_email
+    password = config.sender_password
+
+    # Create the email
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = config.subject
+
+    # Email body
+    body = f"<p>The predictions for your resource {args.resource_id} has been resolved</p>"
+    msg.attach(MIMEText(body, "html"))
+
+    try:
+        # Connect to the server
+        server = smtplib.SMTP(smtp_host, smtp_port)
+        server.starttls()  # Secure the connection
+        server.login(sender_email, password)
+        server.send_message(msg)
+
+        print("✅ Email sent successfully!")
+    except Exception as e:
+        print(f"❌ Error: {e}")
+    finally:
+        server.quit()
+
+def main(args):
+    # get arguments and configure app logger
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    
+    # get service arguments
+    base_file = Path(__file__).resolve().parent.parent.parent
+    model_id = args.model_id    
+    resource_file = args.resource_file
+    user_email = args.user_email
+
+    # get job active profile            
+    if not os.getenv('ARG_PYTHON_PROFILES_ACTIVE'):
+        config = parse_config('./src/wearablepermed_predictor/environment/environment.yaml')        
+    else:
+        config = parse_config('./src/wearablepermed_predictor/environment/environment-' + os.getenv('ARG_PYTHON_PROFILES_ACTIVE') + '.yaml')
+
+    _logger.info("Starting predictor")
+
+    predictor = load_model(model_id, base_file)
+
+    # STEP03: get predictions from resource id
+    _logger.info("STEP03: Get predictions from model id %s ", model_id)
+    predictions = predict_by_resource(resource_file, model_id)
+
+    # STEP04: save resource predictions
+
+    # STEP05: send email to sender user
+    _logger.info("STEP05: Send email for user email %s ", user_email)
+    send_email(args, config, resource_file, user_email)
+
+    _logger.info("Predictor finalized")
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+if __name__ == "__main__":
+    run()        
